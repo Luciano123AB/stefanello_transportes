8ea4188300cbf6b63b7d0d9e30788f845c7f42dd
Luciano Eduardo <luciano123ab@gmail.com>
2026-05-29 16:11:07 -0300

    002(Pagina_Home)

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -1,6 +1,16 @@
-<footer class="d-flex justify-content-center border-top border-5 border-black bg-success p-3 mt-auto">
-    <div class="d-grid text-center">
+<footer id="barra_inferior" class="justify-content-center flex-wrap border-top border-5 border-black bg-success text-center p-3 mt-auto">
+    <div class="d-grid">
         <p>© Todos os direitos reservados: Maurício Stefanello Barbieri</p>
         <p>2026 - {{ date('Y') }} | {{ env('APP_NAME') }}</p>
+    </div>
+
+    <div class="align-self-center">
+        <p>CNPJ: 00.000.000/0000-00</p>
+    </div>
+
+    <div class="d-grid">
+        <label>Contatos:</label>
+        <p>Email: endereco@gmail.com</p>
+        <p>Whatsapp: (99) 99999-9999</p>
     </div>
 </footer>
