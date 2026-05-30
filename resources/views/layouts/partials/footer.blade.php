b96c79c88777ef360f0ff1bd4516a8cee9809a9b
Luciano Eduardo <luciano123ab@gmail.com>
2026-05-30 16:08:57 -0300

    002(Pagina_Home)

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -1,6 +1,6 @@
-<footer id="bottom_bar" class="border-footer justify-content-center flex-wrap border-5 border-black bg-success text-center shadow-lg p-3 mt-auto">
+<footer id="bottom_bar" class="justify-content-center flex-wrap border-5 border-black bg-success text-center shadow-lg p-3 mt-auto">
     <div class="d-grid align-self-center">
-        <p class="fw-bold">
+        <p class="bg-white border border-black fw-bold rounded p-1">
             © Todos os direitos reservados: Maurício Stefanello Barbieri
             <br>
             2026 - {{ date('Y') }} | {{ env('APP_NAME') }}
@@ -8,19 +8,19 @@
     </div>
 
     <div class="align-self-center">
-        <p>
+        <p class="bg-white border border-black rounded p-1">
             <i class="bi bi-building"></i>
             CNPJ: 00.000.000/0000-00
         </p>
     </div>
 
-    <div class="d-grid">
+    <div class="d-grid bg-white border border-black rounded p-1">
         <label class="fw-bold">Contatos:</label>
-        <p>
+        <p class="mb-0">
             <i class="bi bi-envelope-at"></i>
             Email: endereco@gmail.com
         </p>
-        <p>
+        <p class="mb-0">
             <i class="bi bi-whatsapp"></i>
             Whatsapp: (99) 99999-9999
         </p>
--- resources/views/layouts/partials/navbar.blade.php
+++ resources/views/layouts/partials/navbar.blade.php
@@ -1,10 +1,10 @@
-<nav id="border-header" class="navbar border-5 border-black bg-success shadow-lg">
+<nav id="top_bar" class="navbar border-5 border-black bg-success shadow-lg">
     <div class="container-fluid">
-        <div class="d-flex align-items-center gap-1">
-            <img src="{{ asset('assets/images/icons/logo.png') }}" class="animate__animated animate__rollIn animate__infinite" height="45">
+        <div class="d-flex align-items-center border border-black bg-warning rounded">
+            <img src="{{ asset('assets/images/icons/logo.png') }}" class="animate__animated animate__rollIn animate__infinite m-1" height="45">
             <a href="{{ route('home') }}" class="navbar-brand"><span class="fs-4 fw-bold">{{ env('APP_NAME') }}:</span> {{ $pageTitle }}</a>
         </div>
-        <div class="d-flex gap-2 my-2">
+        <div id="buttons_navbar" class="d-flex gap-2">
             <a href="#" class="btn btn-warning border-black shadow-sm">LOGIN</a>
             <a href="#" class="btn btn-warning border-black shadow-sm">CADASTRAR</a>
         </div>
