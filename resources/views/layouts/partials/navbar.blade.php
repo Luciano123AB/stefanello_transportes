d0a259675afbc233c15c9a3ecec4837f38998f03
Luciano Eduardo <luciano123ab@gmail.com>
2026-06-01 19:16:04 -0300

    003(Footer)

--- resources/views/layouts/partials/navbar.blade.php
+++ resources/views/layouts/partials/navbar.blade.php
@@ -1,12 +1,10 @@
 <nav id="top_bar" class="navbar border-5 border-warning bg-success shadow-lg">
     <div class="container-fluid">
-        <div class="buttons_navbar_color d-flex align-items-center border border-black rounded">
-            <img src="{{ asset('assets/images/icons/logo.png') }}" class="animate__animated animate__rollIn animate__infinite m-1" height="45">
-            <a href="{{ route('home') }}" class="navbar-brand">
-                <span class="fs-4 fw-bold">{{ env('APP_NAME') }}:</span>
-                {{ $pageTitle }}
-            </a>
-        </div>
+        <a href="{{ route('home') }}" class="buttons_navbar_color navbar-brand d-flex align-items-center btn btn-warning border border-black rounded p-2">
+            <img src="{{ asset('assets/images/icons/logo.png') }}" class="animate__animated animate__rollIn animate__infinite me-2" height="45">
+            <span class="fs-4 fw-bold">{{ env('APP_NAME') }}:</span>
+            {{ $pageTitle }}
+        </a>
         <div id="buttons_navbar" class="d-flex gap-2">
             <a href="#" class="buttons_navbar_color btn btn-warning border-black shadow-sm">
                 <iconify-icon icon="streamline-color:user-add-plus" class="animate__animated animate__heartBeat animate__infinite"></iconify-icon>
