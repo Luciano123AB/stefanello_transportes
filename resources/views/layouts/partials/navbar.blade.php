3d9720868c8df306dd75b8049a607928f9499869
Luciano Eduardo <luciano123ab@gmail.com>
2026-06-01 16:02:15 -0300

    004(Home)

--- resources/views/layouts/partials/navbar.blade.php
+++ resources/views/layouts/partials/navbar.blade.php
@@ -9,11 +9,11 @@
         </div>
         <div id="buttons_navbar" class="d-flex gap-2">
             <a href="#" class="buttons_navbar_color btn btn-warning border-black shadow-sm">
-                <iconify-icon icon="streamline-color:login-1"></iconify-icon>
+                <iconify-icon icon="streamline-color:login-1" class="animate__animated animate__fadeOutLeft animate__infinite"></iconify-icon>
                 LOGIN
             </a>
             <a href="#" class="buttons_navbar_color btn btn-warning border-black shadow-sm">
-                <iconify-icon icon="streamline-color:user-add-plus"></iconify-icon>
+                <iconify-icon icon="streamline-color:user-add-plus" class="animate__animated animate__heartBeat animate__infinite"></iconify-icon>
                 CADASTRAR
             </a>
         </div>
