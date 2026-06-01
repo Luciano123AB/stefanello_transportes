9872d070bde148c99aa43f52766a4c91916664ae
Luciano123AB <Lucianoedustefa24032004@gmail.com>
2026-06-01 19:57:05 -0300

    Update main_layout.blade.php

--- resources/views/components/main_layout.blade.php
+++ resources/views/components/main_layout.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>{{ env('APP_NAME') }} - {{ $pageTitle }}</title>
+    <title>{{ config('APP_NAME') }} - {{ $pageTitle }}</title>
     <link rel="shortcut icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
 
     @include('layouts.partials.links')
