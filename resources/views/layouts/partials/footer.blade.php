9ac8e67e155ac0a897fc8d2cac17f2f073d5470e
Luciano Eduardo <luciano123ab@gmail.com>
2026-06-01 15:53:56 -0300

    004(Home)

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -1,6 +1,6 @@
 <footer id="bottom_bar" class="justify-content-center flex-wrap border-5 border-black bg-success text-center shadow-lg p-3 mt-auto">
     <div class="d-grid align-self-center">
-        <p class="bg-white border border-black fw-bold rounded p-1">
+        <p class="bg-light border border-black fw-bold rounded p-1">
             © Todos os direitos reservados: Maurício Stefanello Barbieri
             <br>
             2026 - {{ date('Y') }} | {{ env('APP_NAME') }}
@@ -8,13 +8,13 @@
     </div>
 
     <div class="align-self-center">
-        <p class="bg-white border border-black rounded p-1">
+        <p class="bg-light border border-black rounded p-1">
             <iconify-icon icon="streamline-emojis:office-building"></iconify-icon>
             CNPJ: 00.000.000/0000-00
         </p>
     </div>
 
-    <div id="contacts" class="d-grid bg-white border border-black rounded p-1">
+    <div id="contacts" class="d-grid bg-light border border-black rounded p-1">
         <label class="fw-bold text-center">Contatos:</label>
         <div>
             <iconify-icon icon="logos:whatsapp-icon"></iconify-icon>
--- resources/views/layouts/partials/navbar.blade.php
+++ resources/views/layouts/partials/navbar.blade.php
@@ -1,15 +1,18 @@
 <nav id="top_bar" class="navbar border-5 border-black bg-success shadow-lg">
     <div class="container-fluid">
-        <div class="d-flex align-items-center border border-black bg-warning rounded">
+        <div class="buttons_navbar_color d-flex align-items-center border border-black rounded">
             <img src="{{ asset('assets/images/icons/logo.png') }}" class="animate__animated animate__rollIn animate__infinite m-1" height="45">
-            <a href="{{ route('home') }}" class="navbar-brand"><span class="fs-4 fw-bold">{{ env('APP_NAME') }}:</span> {{ $pageTitle }}</a>
+            <a href="{{ route('home') }}" class="navbar-brand">
+                <span class="fs-4 fw-bold">{{ env('APP_NAME') }}:</span>
+                {{ $pageTitle }}
+            </a>
         </div>
         <div id="buttons_navbar" class="d-flex gap-2">
-            <a href="#" class="btn btn-warning border-black shadow-sm">
+            <a href="#" class="buttons_navbar_color btn btn-warning border-black shadow-sm">
                 <iconify-icon icon="streamline-color:login-1"></iconify-icon>
                 LOGIN
             </a>
-            <a href="#" class="btn btn-warning border-black shadow-sm">
+            <a href="#" class="buttons_navbar_color btn btn-warning border-black shadow-sm">
                 <iconify-icon icon="streamline-color:user-add-plus"></iconify-icon>
                 CADASTRAR
             </a>
