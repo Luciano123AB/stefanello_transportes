28772b808107a7748e0bb3865df88aa00289c0ff
Luciano Eduardo <luciano123ab@gmail.com>
2026-06-01 16:04:33 -0300

    003(Footer)

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,15 @@
 <?php
 
 use App\Http\Controllers\MainController;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('/')->group(function () {
     Route::controller(MainController::class)->group(function () {
         Route::get('/', 'home')->name('home');
+    });
+
+    Route::fallback(function(): RedirectResponse {
+        return redirect()->route('home');
     });
 });
