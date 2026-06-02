a05db63dea9e6944ddcee7316717fc54429049cc
Luciano Eduardo <luciano123ab@gmail.com>
2026-06-02 12:28:00 -0300

    005(DataBase)

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,6 +6,7 @@ use Carbon\Carbon;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,7 +19,7 @@ class DatabaseSeeder extends Seeder
             'name' => 'MBarbieri273',
             'email' => 'mbarbieri273@gmail.com',
             'email_verified_at' => Carbon::now(),
-            'password' => bcrypt('150177Mb'),
+            'password' => Hash::make('150177Mb'),
             'created_at' => Carbon::now()
         ]);
     }
