1714a131dfc27734beac4d6fee07d6f2e50779cc
Luciano Eduardo <luciano123ab@gmail.com>
2026-06-05 18:11:02 -0300

    009(Mais_Informacoes)

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,5 +1,9 @@
 <x-main_layout :pageTitle="'Cadastro'">
     <h1 class="text-center fw-bold mb-5">Cadastro</h1>
     
-    @livewire('⚡form_register')
+    <form action="{{ route('register') }}" method="post" class="card d-grid gap-3 border border-black bg-warning shadow p-3">
+        @csrf
+
+        @livewire('⚡form_register')
+    </form>
 </x-main_layout>
