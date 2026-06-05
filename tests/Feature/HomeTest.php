a779d59299e44a76f154be52a9d73850527fba4a
Luciano Eduardo <luciano123ab@gmail.com>
2026-06-05 10:13:02 -0300

    006(Login)

--- tests/Feature/HomeTest.php
+++ tests/Feature/HomeTest.php
@@ -1,11 +1,15 @@
 <?php
 
-describe('Testes do MainController', function () {
-    it('testar acesso ao Home', function () {
+describe('Testes do home', function () {
+    it('testar acesso ao home', function () {
         expect($this->get('/')->status())->toBe(200);
     });
 
-    it('testar carregamento da view do Home', function () {
+    it('testar carregamento da view do home', function () {
         expect($this->get('/')->assertViewIs('home'));
+    });
+
+    it('testar conteúdo do home', function () {
+        expect($this->get('/')->assertSee('Seja BEM-VINDO!'));
     });
 });
