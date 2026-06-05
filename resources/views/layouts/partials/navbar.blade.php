5c664090805de7b0c7c4e56d58de3619070d79ed
Luciano Eduardo <luciano123ab@gmail.com>
2026-06-05 18:00:53 -0300

    002(Navbar)

--- resources/views/layouts/partials/navbar.blade.php
+++ resources/views/layouts/partials/navbar.blade.php
@@ -1,6 +1,6 @@
 <nav id="top_bar" class="navbar border-5 border-warning bg-success shadow-lg">
     <div class="container-fluid">
-        <a href="{{ route('home') }}" class="buttons_navbar_color navbar-brand d-flex gap-2 align-items-center btn btn-warning border border-black rounded p-2">
+        <a href="{{ route('home') }}" class="buttons_navbar_color navbar-brand d-flex gap-2 flex-wrap align-items-center btn btn-warning border border-black rounded p-2">
             <img src="{{ asset('assets/images/icons/logo.png') }}" class="animate__animated animate__rollIn animate__infinite" height="45">
             <span class="fs-4 fw-bold">{{ config('app.name') }}:</span>
             {{ $pageTitle }}
