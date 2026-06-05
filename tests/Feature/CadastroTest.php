571008850ba6bb6f663ee0b3dde7e0379fbccaf9
Luciano Eduardo <luciano123ab@gmail.com>
2026-06-05 10:11:53 -0300

    007(Cadastro)

---
+++ tests/Feature/CadastroTest.php
@@ -0,0 +1,27 @@
+<?php
+
+describe('Testes do cadastro', function () {
+    it('testar acesso ao cadastro', function () {
+        expect($this->get('/register')->status())->toBe(200);
+    });
+
+    it('testar carregamento da view do cadastro', function () {
+        expect($this->get('/register')->assertViewIs('auth.register'));
+    });
+
+    it('testar o cadastro do usuário', function () {
+
+        $username = 'Maurício Barbieri';
+        $email = 'mbarbieri273@gmail.com';
+        $password = '@150177Mb';
+        $result = $this->post('/register', [
+            'name' => $username,
+            'email' => $email,
+            'password' => $password,
+            'password_confirmation' => $password,
+            'role' => 'admin'
+        ])->assertRedirect('/home');
+
+        expect($result->status())->toBe(302);
+    });
+});
--- tests/Feature/LoginTest.php
+++ tests/Feature/LoginTest.php
@@ -4,7 +4,7 @@ use App\Models\User;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Hash;
 
-describe('Testes do login.', function () {
+describe('Testes do login', function () {
     it('testar acesso ao login', function () {
         expect($this->get('/login')->status())->toBe(200);
     });
@@ -13,9 +13,9 @@ describe('Testes do login.', function () {
         expect($this->get('/login')->assertViewIs('auth.login'));
     });
 
-    it('testar o login do usuário.', function() {
+    it('testar o login do usuário', function() {
         User::create([
-            'name' => 'Matheus Barbieri',
+            'name' => 'Maurício Barbieri',
             'email' => 'mbarbieri273@gmail.com',
             'email_verified_at' => Carbon::now(),
             'password' => Hash::make('150177Mb'),
