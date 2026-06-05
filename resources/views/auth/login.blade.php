aceebc0b7f85a537e3cb637de715e792cf2f475d
Luciano Eduardo <luciano123ab@gmail.com>
2026-06-05 18:32:09 -0300

    006(Login)

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,9 +1,5 @@
 <x-main_layout :pageTitle="'Login'">
     <h1 class="text-center fw-bold mb-5">Login</h1>
     
-    <form action="{{ route('login') }}" method="post" class="card d-grid gap-3 border border-black bg-warning shadow p-3">
-        @csrf
-        
-        @livewire('⚡form_login')
-    </form>
+    @livewire('⚡form_login')    
 </x-main_layout>
