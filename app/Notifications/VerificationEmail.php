e69b3ff92029804a788374f995e2379d2cc7a553
Luciano Eduardo <luciano123ab@gmail.com>
2026-06-07 16:46:52 -0300

    008(Verificacao)

--- app/Notifications/VerificationEmail.php
+++ app/Notifications/VerificationEmail.php
@@ -3,6 +3,7 @@
 namespace App\Notifications;
 
 use Carbon\Carbon;
+use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
@@ -10,6 +11,12 @@ use Illuminate\Support\Facades\URL;
 
 class VerificationEmail extends Notification implements ShouldQueue
 {
+    
+    use Queueable;
+
+    public int $tries = 3;
+    public int $timeout = 60;
+
     /**
      * Create a new notification instance.
      */
