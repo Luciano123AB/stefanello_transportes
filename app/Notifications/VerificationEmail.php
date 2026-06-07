3cc552ec142975eb2bae925e7cf4a43a587db46c
Luciano Eduardo <luciano123ab@gmail.com>
2026-06-07 14:58:11 -0300

    Railway

--- app/Notifications/VerificationEmail.php
+++ app/Notifications/VerificationEmail.php
@@ -3,15 +3,12 @@
 namespace App\Notifications;
 
 use Carbon\Carbon;
-use Illuminate\Bus\Queueable;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 use Illuminate\Support\Facades\URL;
 
 class VerificationEmail extends Notification
 {
-    use Queueable;
-
     /**
      * Create a new notification instance.
      */
