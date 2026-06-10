cc8b285c0879fd4251681896725952e1c1ac8a0c
Luciano Eduardo <luciano123ab@gmail.com>
2026-06-10 12:06:40 -0300

    010(Melhorias)

--- resources/views/layouts/partials/navbar.blade.php
+++ resources/views/layouts/partials/navbar.blade.php
@@ -1,6 +1,6 @@
 <nav id="top_bar" class="navbar border-5 border-warning bg-success shadow-lg">
     <div class="container-fluid">
-        <a href="{{ route('home') }}" class="buttons_navbar_color navbar-brand d-flex flex-wrap align-items-center btn btn-warning border border-black rounded p-2">
+        <a href="{{ route('home') }}" class="buttons_navbar_color navbar-brand d-flex flex-wrap align-items-center btn btn-warning border border-black focus-ring focus-ring-warning rounded p-2">
             <img src="{{ asset('assets/images/icons/logo.png') }}" class="animate__animated animate__rollIn animate__infinite me-2" height="45">
             <span class="fs-4 fw-bold me-2">{{ config('app.name') }}:</span>
             <span id="page_title">{{ $pageTitle }}</span>
@@ -8,13 +8,13 @@
         <div id="buttons_navbar" class="d-flex gap-2">
             @guest
                 @if ($pageTitle !== 'Cadastro')
-                    <a href="{{ route('register') }}" class="buttons_navbar_color btn btn-warning border-black shadow-sm">
+                    <a href="{{ route('register') }}" class="buttons_navbar_color btn btn-warning border-black focus-ring focus-ring-warning shadow-sm">
                         <iconify-icon icon="streamline-color:user-add-plus" class="animate__animated animate__heartBeat animate__infinite"></iconify-icon>
                         CADASTRAR
                     </a>
                 @endif
                 @if ($pageTitle !== 'Login')
-                    <a href="{{ route('login') }}" class="buttons_navbar_color btn btn-warning border-black shadow-sm">
+                    <a href="{{ route('login') }}" class="buttons_navbar_color btn btn-warning border-black focus-ring focus-ring-warning shadow-sm">
                         <iconify-icon icon="streamline-color:login-1" class="animate__animated animate__fadeOutLeft animate__infinite"></iconify-icon>
                         ENTRAR
                     </a>
