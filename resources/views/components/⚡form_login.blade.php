b662c4e5a4537af5d1389c7d99a1c2614c9750b2
Luciano Eduardo <luciano123ab@gmail.com>
2026-06-10 12:28:35 -0300

    010(Melhorias)

--- resources/views/components/⚡form_login.blade.php
+++ resources/views/components/⚡form_login.blade.php
@@ -60,17 +60,17 @@ new class extends Component {
     </div>
 
     <div class="card-footer d-flex justify-content-between bg-warning-subtle">
-        <a href="{{ route('home') }}" class="buttons_navbar_color btn btn-warning border-black align-self-baseline focus-ring focus-ring-warning shadow-sm">
+        <a href="{{ route('home') }}" class="buttons_navbar_color btn btn-warning border-black align-self-baseline focus-ring focus-ring-warning">
             <iconify-icon icon="twemoji:left-arrow" class="animate__animated animate__fadeOutLeft animate__infinite"></iconify-icon>
             VOLTAR
         </a>
 
         <div id="buttons_form" class="gap-1">
-            <button type="button" class="btn btn-danger border-black focus-ring focus-ring-danger shadow-sm" wire:click="clearInputs()">
+            <button type="button" class="btn btn-danger border-black focus-ring focus-ring-danger" wire:click="clearInputs()">
                 <iconify-icon icon="glyphs-poly:trash-1" class="animate__animated animate__bounceOut animate__infinite"></iconify-icon>
                 LIMPAR
             </button>
-            <button type="submit" class="btn btn-success border-black focus-ring focus-ring-success shadow-sm">
+            <button type="submit" class="btn btn-success border-black focus-ring focus-ring-success">
                 <iconify-icon icon="streamline-color:login-1" class="animate__animated animate__fadeOutLeft animate__infinite"></iconify-icon>
                 LOGAR
             </button>
