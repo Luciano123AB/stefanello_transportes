633565602ce67689544719f758912cc43c2d4bc5
Luciano Eduardo <luciano123ab@gmail.com>
2026-06-12 18:01:41 -0300

    Railway

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Archives;
 use App\Http\Controllers\MainController;
 use App\Http\Controllers\DataUpdate;
 use App\Http\Controllers\UserDelete;
+use App\Http\Middleware\IsAdmin;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Route;
 
@@ -16,7 +17,9 @@ Route::prefix('/')->group(function () {
 
             Route::get('edit-profile', 'editProfile')->name('edit.profile');
 
-            Route::get('file-list', 'fileList')->name('file.list');
+            Route::middleware([IsAdmin::class])->group(function () {
+                Route::get('file-list', 'fileList')->name('file.list');
+            });
         });
     });
 
@@ -37,7 +40,7 @@ Route::prefix('/')->group(function () {
     });
 
     Route::controller(Archives::class)->group(function () {
-        Route::middleware(['auth', 'verified'])->group(function () {            
+        Route::middleware(['auth', 'verified', IsAdmin::class])->group(function () {            
             Route::post('file-upload', 'fileUpload')->name('file.upload');
             Route::get('file-view/{name}', 'fileView')->name('file.view');
             Route::get('file-download/{name}', 'fileDownload')->name('file.download');
